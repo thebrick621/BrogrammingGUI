e9c83f093b0e06a6d2c9d39834ea0ce4513b539a
Preston <thebrick621@gmail.com>
2022-03-09 15:18:05 -0800

    Started to implement save func, bugged though and needs to have try: added to pass through when savefile not created. Also, save file isn't erasing properly and I have 0 clue why, requires lots of debugging

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import json
 # Initializing Pygame Library
 pygame.init()
 pygame.font.init()
@@ -49,6 +50,7 @@ yellow = (255,255,0)
 light_yellow = (255,228,138)
 dark_grey = (78,78,78)
 
+# Flashy Text Stuff
 flashcount = 0
 flashcolors = [black,light_green]
 flashcolors2 = [black,green]
@@ -56,6 +58,11 @@ flashcolors2 = [black,green]
 # Title & Icon Data
 pygame.display.set_caption("Brogramming")
 pygame.display.set_icon(icon)
+
+# Save Stuff
+savedata = {
+    'Firstsave' : False
+}
 
 # Draw rectangles
 def draw_rect(x, y, w, h, color):
@@ -95,6 +102,8 @@ def start_button(msg,x,y,w,h,inactive,active,action=None):
     	draw_rect(x,y,w,h, active) # If yPOS plus width is greater than mouse Y then you are hovering over rect.
     	screen.blit(smenu_buttonhi, (x-30,y-75))
 
+        # Found a bug here; not sure why but executing the function after clicking isn't 100% working. Kinda works?
+        # Switching game to not rely on buttons but will use for ez development & navi
     	if click[0] == 1 and action != None:
             action() # Does whatever our action parameter is so long as its a function!
     else:   	
@@ -105,17 +114,108 @@ def start_button(msg,x,y,w,h,inactive,active,action=None):
     textRect.center = ((x+(w/2), (y+(h/2)))) # Find the center by adding X plus width/2 same w/ Y value but w/ height
     screen.blit(textSurf, textRect) # Draws our Text
 
-# Load Character Img
 def player(x,y):
+# Load Character Img
     screen.blit(charsprite, (x,y))
+
+def scene_open():
+    print('null')
+def loadsave():
+    print('null')
+def delsave_confirm():
+    loop = True
+    usertext_input = ''
+    inputbox = pygame.Rect(350,100, 600,400) #POS of input box
+    inputmargins = (inputbox.x + 5, inputbox.y + 150)
+    outputmargins = (inputbox.x + 5, inputbox.y + 210)
+    # Gotta do it global to keep the color shift throughout the game *Sadge*
+    global flashcount
+    global flashcolors
+    global flashcolors2
+    
+    while loop:
+
+        # For loop is for any EVENTS
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                # Autosaves on quit
+                with open('mysavedata.txt', 'w') as savefile:
+                    json.dump(savedata, savefile)
+                quit_game()
+
+
+            if event.type == pygame.KEYDOWN:
+
+                    if event.key == pygame.K_BACKSPACE:
+                        # Converts all input txt to lowercase for easier command struc
+                        usertext_input = usertext_input[:-1]
+                    
+                    # BIGGGGG Elif to link to commands from here incl. cheats & EEs as well as any other menu commands
+                    elif event.key == pygame.K_RETURN:
+                        usertext_input = usertext_input.lower()
+
+                        # Add if statements to do something if certain str is entered
+                        if 'no' in usertext_input or 'n' in usertext_input:
+                            start_game()
+
+                        elif 'yes' in usertext_input or 'y' in usertext_input:
+                            savedata.clear()
+                            savedata['Firstave']=False
+                            with open('mysavedata.txt', 'w') as savefile:
+                                json.dump(savedata, savefile)
+                            screen.blit(mediumstitchText.render('Deleted',True, flashcolors[1]),(outputmargins))
+                            pygame.display.update()
+                            pygame.time.delay(1500)
+                            start_game()
+
+                        # If input = dummystr then clear text
+                        del usertext_input
+                        usertext_input = ''
+
+                    # Sets max len of input in input box
+                    elif len(usertext_input) == 19:
+                        usertext_input = usertext_input[:-1]
+                    # Writes on the screen
+                    else:
+                        usertext_input += event.unicode
+
+            # Allows us to select & Deselect text box; maybe can make this a function?
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if inputbox.collidepoint(event.pos):
+                    box_selected = True
+                else:
+                    box_selected = False
+
+        # Dispalys Background pic
+        screen.blit(bedroom_back, (0,0))
+
+        #screen.blit(titleimg, (250,0))
+        flashcount += 1
+        
+        # Blits the main input box and usr text
+        usertext_surface = largestitchText.render(usertext_input,True,flashcolors[(flashcount%2)])
+        titletext = hugestitchText.render('Save Being Erased',True,flashcolors2[(flashcount%2)])
+        default_cmdline = mediumstitchText.render('ARE YOU SURE?',True,flashcolors2[(flashcount%2)])
+        pygame.draw.rect(screen,black,inputbox)
+        screen.blit(titletext, (inputbox.x , inputbox.y + 5))
+        screen.blit(default_cmdline, (inputbox.x + 5, inputbox.y + 105))
+        screen.blit(usertext_surface, (inputmargins))
+
+
+        # Updates Screen & Sets Framerate
+        pygame.display.update()
+        clock.tick(60)
 
 def start_game():
     intro = True
     usertext_input = ''
     inputbox = pygame.Rect(350,100, 600,400) #POS of input box
-    box_selected = False
-    inputmargins = (inputbox.x + 90, inputbox.y + 150)
-    outputmargins = (inputbox.x + 5, inputbox.y + 250)
+    inputmargins = (inputbox.x + 5, inputbox.y + 150)
+    outputmargins = (inputbox.x + 5, inputbox.y + 210)
+    with open('mysavedata.txt') as savefile:
+        savedata = json.load(savefile)
+
+    # Gotta do it global to keep the color shift throughout the game *Sadge*
     global flashcount
     global flashcolors
     global flashcolors2
@@ -125,12 +225,13 @@ def start_game():
         # For loop is for any EVENTS
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                # Autosaves on quit
+                with open('mysavedata.txt', 'w') as savefile:
+                    json.dump(savedata, savefile)
                 quit_game()
 
 
             if event.type == pygame.KEYDOWN:
-
-                if box_selected == True:
 
                     if event.key == pygame.K_BACKSPACE:
                         # Converts all input txt to lowercase for easier command struc
@@ -142,24 +243,39 @@ def start_game():
 
                         # Add if statements to do something if certain str is entered
                         if 'info' in usertext_input or 'nfo' in usertext_input:
-                            screen.blit(mediumstitchText.render('Created by Preston Philbrick 2022',True, flashcolors[(flashcount%2)]),(outputmargins))
+                            screen.blit(mediumstitchText.render('Created by Preston Philbrick 2022',True, flashcolors[1]),(outputmargins))
                             pygame.display.update()
-                            time.sleep(2)
+                            pygame.time.delay(2000)
 
-                        elif 'quit' in usertext_input:
+                        elif 'quit' in usertext_input or 'back' in usertext_input or 'exit' in usertext_input:
                             screen.blit(mediumstitchText.render('Goodbye',True, flashcolors[1]),(outputmargins))
-                            pygame.display.update() 
+                            pygame.display.update()
                             game_intro()
 
+                        elif 'startgame' in usertext_input or 'start' in usertext_input or 'start game' in usertext_input:
+                            
+                            if ('Firstsave', False) in savedata.items():
+                                savedata['Firstsave'] = True
+                                scene_open()
 
-                        elif 'redtext' in usertext_input or 'red text' in usertext_input:
-                            del usertext_input
-                            usertext_input = ''
-                            screen.blit(mediumstitchText.render('Updated',True, flashcolors[1]),(outputmargins))
+                            elif ('Firstsave', True) in savedata.items():
+                                loadsave()
+
+                        elif 'erasesave' in usertext_input or 'erase' in usertext_input or 'erase save' in usertext_input:
+                            
+                            if ('Firstsave', False) in savedata.items():
+                                screen.blit(mediumstitchText.render('No Save Data Found',True, flashcolors[1]),(outputmargins))
+                                pygame.display.update()
+                                pygame.time.delay(2000)
+
+                            if ('Firstsave', True) in savedata.items():
+                                delsave_confirm()
+
+                        elif 'help' in usertext_input:
+                            screen.blit(mediumstitchText.render('StartGame     Info',True, magenta),(inputbox.x + 110, inputbox.y + 210))
+                            screen.blit(mediumstitchText.render('EraseSave    Back',True, magenta),(inputbox.x + 110, inputbox.y + 260))
                             pygame.display.update()
-                            time.sleep(2)
-                            flashcolors = [black, light_red]
-                            flashcolors2 = [black, red]
+                            pygame.time.delay(2000)
 
                         # If input = dummystr then clear text
                         del usertext_input
@@ -188,7 +304,7 @@ def start_game():
         # Blits the main input box and usr text
         helptext = mediumstitchText.render('Type Help For Commands', True, flashcolors[(flashcount%2)])
         usertext_surface = largestitchText.render(usertext_input,True,flashcolors[(flashcount%2)])
-        titletext = hugestitchText.render('New Game',True,flashcolors2[(flashcount%2)])
+        titletext = hugestitchText.render('Start Game',True,flashcolors2[(flashcount%2)])
         default_cmdline = mediumstitchText.render('What would you like to do?',True,flashcolors2[(flashcount%2)])
         pygame.draw.rect(screen,black,inputbox)
         screen.blit(helptext, (inputbox.x + 110, inputbox.y + 350))
@@ -206,8 +322,8 @@ def game_intro():
     usertext_input = ''
     inputbox = pygame.Rect(350,100, 600,400) #POS of input box
     box_selected = False
-    inputmargins = (inputbox.x + 90, inputbox.y + 150)
-    outputmargins = (inputbox.x + 5, inputbox.y + 250)
+    inputmargins = (inputbox.x + 5, inputbox.y + 150)
+    outputmargins = (inputbox.x + 5, inputbox.y + 210)
     global flashcount
     global flashcolors
     global flashcolors2
@@ -217,8 +333,10 @@ def game_intro():
         # For loop is for any EVENTS
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                # Autosaves on quit
+                with open('mysavedata.txt', 'w') as savefile:
+                    json.dump(savedata, savefile)
                 quit_game()
-
 
             if event.type == pygame.KEYDOWN:
 
@@ -236,18 +354,24 @@ def game_intro():
                         if 'info' in usertext_input or 'nfo' in usertext_input:
                             screen.blit(mediumstitchText.render('Created by Preston Philbrick 2022',True, flashcolors[(flashcount%2)]),(outputmargins))
                             pygame.display.update()
-                            time.sleep(2)
+                            pygame.time.delay(1250)
+
 
                         elif 'quit' in usertext_input:
                             screen.blit(mediumstitchText.render('Goodbye',True, flashcolors[1]),(outputmargins))
                             pygame.display.update() 
-                            time.sleep(1)
+                            pygame.time.delay(1000)
                             quit_game()
+
+                        elif 'help' in usertext_input:
+                            screen.blit(mediumstitchText.render('Quit   Info   Start',True, magenta),(outputmargins))
+                            pygame.display.update()
+                            pygame.time.delay(2000)
 
                         elif 'start' in usertext_input or 'start game' in usertext_input or 'startgame' in usertext_input:
                             del usertext_input
                             usertext_input = ''
-                            time.sleep(0.25)
+                            pygame.time.delay(250)
                             start_game()
 
                         elif 'redtext' in usertext_input or 'red text' in usertext_input:
@@ -255,7 +379,7 @@ def game_intro():
                             usertext_input = ''
                             screen.blit(mediumstitchText.render('Updated',True, flashcolors[1]),(outputmargins))
                             pygame.display.update()
-                            time.sleep(2)
+                            pygame.time.delay(2000)
                             flashcolors = [black, light_red]
                             flashcolors2 = [black, red]
 
@@ -264,7 +388,7 @@ def game_intro():
                             usertext_input = ''
                             screen.blit(mediumstitchText.render('Updated',True, flashcolors[1]),(outputmargins))
                             pygame.display.update()
-                            time.sleep(2)
+                            pygame.time.delay(2000)
                             flashcolors = [black, light_blue]
                             flashcolors2 = [black, blue]
 
@@ -273,7 +397,7 @@ def game_intro():
                             usertext_input = ''
                             screen.blit(mediumstitchText.render('Updated',True, flashcolors[1]),(outputmargins))
                             pygame.display.update()
-                            time.sleep(2)
+                            pygame.time.delay(2000)
                             flashcolors = [black, light_pink]
                             flashcolors2 = [black, magenta]
 
@@ -282,7 +406,7 @@ def game_intro():
                             usertext_input = ''
                             screen.blit(mediumstitchText.render('Updated',True, flashcolors[1]),(outputmargins))
                             pygame.display.update()
-                            time.sleep(2)
+                            pygame.time.delay(2000)
                             flashcolors = [black, light_yellow]
                             flashcolors2 = [black, yellow]
 
@@ -291,7 +415,7 @@ def game_intro():
                             usertext_input = ''
                             screen.blit(mediumstitchText.render('Updated',True, flashcolors[1]),(outputmargins))
                             pygame.display.update()
-                            time.sleep(2)
+                            pygame.time.delay(2000)
                             flashcolors = [black, light_green]
                             flashcolors2 = [black, green]
 
